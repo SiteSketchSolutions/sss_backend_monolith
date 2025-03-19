86e8539f9738b6f19d321bf5d16b56635040a4b8
Akashrp512 <akashrp512@gmail.com>
2025-03-20 00:05:49 +0530

    Added referenceId as optional

--- app/routes/v1/partPaymentStageRoutes.js
+++ app/routes/v1/partPaymentStageRoutes.js
@@ -12,7 +12,7 @@ let routes = [
                 stageId: Joi.number().required().description("Enter stage id"),
                 amount: Joi.number().required().description("Enter part payment amount"),
                 method: Joi.string().required().description("Enter payment method like upi or card"),
-                referenceId: Joi.string().required().description("Enter payment reference id"),
+                referenceId: Joi.string().description("Enter payment reference id"),
                 note: Joi.string().description("Additional notes for the payment")
             },
             group: "PartPaymentStage",
